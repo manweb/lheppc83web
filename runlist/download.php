716e8fedbc3cd9c4fd6c70efa7327874092a42c3
Manuel <weber_manuel@hotmail.com>
2013-06-07 09:49:01 +0000

    This cookie is now defined in the index file.

--- runlist/download.php
+++ runlist/download.php
@@ -1,19 +1,5 @@
 <?php
 
-if (!$_COOKIE['experiment']) {setcookie("experiment", "xgt", time()+86400);}
-
-//**** Home directory ****
-switch ($_COOKIE['experiment']) {
-    case "xgt":
-        $homeDir = "/xgt/";
-        break;
-    case "xlr":
-        $homeDir = "/xlr/";
-        break;
-    default:
-        $homeDir = "/xgt";
-}
-
 $downloadfile = $homeDir.$_GET["id"];
 $filename = $_GET["id"];
 $filesize = filesize($downloadfile);
