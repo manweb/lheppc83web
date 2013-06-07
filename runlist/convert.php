14f3051464fb7ab6c135ecc7b0dec27757f7f6be
Manuel <weber_manuel@hotmail.com>
2013-06-07 10:47:33 +0000

    Bug fix.

--- runlist/convert.php
+++ runlist/convert.php
@@ -1,8 +1,22 @@
 <?php
 
+if (!$_COOKIE['experiment']) {$exp = "xgt";}
+else {$exp = $_COOKIE['experiment'];}
+
+//**** Home directory ****
+switch ($exp) {
+    case "xgt":
+        $homeDir = "/xgt/";
+        break;
+    case "xlr":
+        $homeDir = "/xlr/";
+        break;
+    default:
+        $homeDir = "/xgt/";
+}
+
 $runNb = $_GET["id"];
 
-$homeDir = "/xgt/";
 $LibExport = "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/exodaq/ReadMidas/lib";
 $PathExport = "export PATH=$PATH:/home/exodaq/ReadMidas/bin";
 $file = $runNb.".mid";
