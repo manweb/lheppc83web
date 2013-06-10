797fd2ed7dba76b3ae3b7b98a5fd39c325db589b
manweb <manuel.weber@lhep.unibe.ch>
2013-06-10 15:30:03 +0200

    Updated IssueTracker.php

--- issuetracker/IssueTracker.php
+++ issuetracker/IssueTracker.php
@@ -24,17 +24,17 @@ $result = mysql_query("select * from issuetracker order by category, date");
 
 while ($issues = mysql_fetch_assoc($result)) {
   echo "<tr>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'><a href='javascript:changeVis(\"div".$issue['ID']."\")'>".$issue['ID']."</a></font>\n";
+  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'><a href='javascript:changeVis(\"div".$issues['ID']."\")'>".$issues['ID']."</a></font>\n";
   echo "</td>\n";
-  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issue['Project']."</font>\n";
+  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issues['Project']."</font>\n";
   echo "</td>\n";
-  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issue['SubmitBy']."</font>\n";
+  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issues['SubmitBy']."</font>\n";
   echo "</td>\n";
-  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='85' height='30' bgcolor='#FFFFFF'><font size='2'>".$issue['AssignedTo']."</font>\n";
+  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='85' height='30' bgcolor='#FFFFFF'><font size='2'>".$issues['AssignedTo']."</font>\n";
   echo "</td>\n";
-  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='75' height='30' bgcolor='#FFFFFF'><font size='2'>".$issue['Description']."</font>\n";
+  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='75' height='30' bgcolor='#FFFFFF'><font size='2'>".$issues['Description']."</font>\n";
   echo "</td>\n";
-  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issue['DueOn']."</font>\n";
+  echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issues['DueOn']."</font>\n";
   echo "</td>\n";
   echo "</tr>\n";
   echo "<div id='div".$issues['ID']."' style='display:none'>More content</div>\n";
