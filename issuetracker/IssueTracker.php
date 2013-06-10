38cabb5fa746d4fd66539e46cb7abbf6a9e7cc17
Manuel <weber_manuel@hotmail.com>
2013-06-10 16:49:35 +0300

    Update IssueTracker.php

--- issuetracker/IssueTracker.php
+++ issuetracker/IssueTracker.php
@@ -11,6 +11,9 @@ function changeVis(id) {
 
 <table align="center" border="0" bordercolor="#000000" cellpadding="0" cellspacing="0" style="border-collapse: collapse; " width="760">
 <tr>
+<td>
+<table align="center" border="0" bordercolor="#000000" cellpadding="0" cellspacing="0" style="border-collapse: collapse; " width="760">
+<tr>
 <td valign="middle" align="left" style="border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1" width="80" height="30" bgcolor="#FFFFFF"><font size="2">Issue#</font>
 </td>
 <td valign="middle" align="center" style="border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1" width="80" height="30" bgcolor="#FFFFFF"><font size="2">Project</font>
@@ -24,6 +27,8 @@ function changeVis(id) {
 <td valign="middle" align="center" style="border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1" width="80" height="30" bgcolor="#FFFFFF"><font size="2">Due on</font>
 </td>
 </tr>
+</table>
+</td>
 
 <?php
 
@@ -34,6 +39,8 @@ mysql_select_db("exo");
 $result = mysql_query("select * from issuetracker order by category, date");
 
 while ($issues = mysql_fetch_assoc($result)) {
+  echo "<td>\n";
+  echo "<table align='center' border='0' bordercolor='#000000' cellpadding='0' cellspacing='0' style='border-collapse: collapse; ' width='760'>\n";
   echo "<tr>\n";
   echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'><a href='javascript:changeVis(\"div".$issues['ID']."\")'>".$issues['ID']."</a></font>\n";
   echo "</td>\n";
@@ -48,10 +55,16 @@ while ($issues = mysql_fetch_assoc($result)) {
   echo "<td valign='middle' align='center' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: solid; border-bottom-width: 1' width='80' height='30' bgcolor='#FFFFFF'><font size='2'>".$issues['DueOn']."</font>\n";
   echo "</td>\n";
   echo "</tr>\n";
+  echo "</table>\n";
+  echo "</td>\n";
+  echo "<td>\n";
   echo "<div id='div".$issues['ID']."' style='display:none'>More content</div>\n";
+  echo "</td>\n";
 }
 
 ?>
+
+</tr>
 
 </table>
 
