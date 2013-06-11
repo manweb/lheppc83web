c6d2b61eb3ff91a4c419424426cce10e5179d7b8
Manuel <weber_manuel@hotmail.com>
2013-06-11 13:35:58 +0200

    Update IssueTracker.php

--- issuetracker/IssueTracker.php
+++ issuetracker/IssueTracker.php
@@ -112,7 +112,7 @@ while ($issues = mysql_fetch_assoc($result)) {
             echo "<tr>\n";
             echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: dashed; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0;' width='40' bgcolor='$bgcolor'>\n";
             echo "</td>\n";
-            echo "<td valign='top' align='left' style='border-color: #0000FF; border-left-style: dashed; border-left-width: 1; border-right-style: dashed; border-right-width: 1; border-top-style: dashed; border-top-width: 1; border-bottom-style: dashed; border-bottom-width: 1' width='660' bgcolor='#E2E2E2'><font size='2'>".$com['SubmitBy']." on ".$com['date'].":<br><br>".nl2br($com['Message'])."</font>\n";
+            echo "<td valign='top' align='left' style='border-color: #0000FF; border-left-style: dashed; border-left-width: 1; border-right-style: dashed; border-right-width: 1; border-top-style: dashed; border-top-width: 1; border-bottom-style: dashed; border-bottom-width: 1' width='660' bgcolor='#E2E2E2'><font size='2'>".$com['SubmitBy']." on ".$com['date']." ".$com['time'].":<br><br>".nl2br($com['Message'])."</font>\n";
             echo "<br>\n";
             echo "</td>\n";
             echo "<td valign='top' align='left' style='border-color: #000000; border-left-style: dashed; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='20' bgcolor='$bgcolor'><img src='../image/delete_comment.png'>\n";
@@ -125,11 +125,11 @@ while ($issues = mysql_fetch_assoc($result)) {
   echo "</div>\n";
   echo "<div id='div".$issues['ID']."_2' style='display:none'>\n";
   echo "<form action='issuetracker/enter_issue.php' class='fileform' method='post' enctype='multipart/form-data'>\n";
-  echo "<table align='right' border='0' bordercolor='#000000' cellpadding='0' cellspacing='0' style='border-collapse: collapse; margin-bottom: 10' width='720'>\n";
+  echo "<table align='right' border='0' bordercolor='#000000' cellpadding='10' cellspacing='0' style='border-collapse: collapse; margin-bottom: 5' width='720'>\n";
   echo "<tr>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='40' bgcolor='$bgcolor'>\n";
+  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: dashed; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0;' width='40' bgcolor='$bgcolor'>\n";
   echo "</td>\n";
-  echo "<td valign='top' align='left' style='border-color: #0000FF; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='660' bgcolor='$bgcolor'>\n";
+  echo "<td valign='top' align='left' style='border-color: #0000FF; border-left-style: dashed; border-left-width: 1; border-right-style: dashed; border-right-width: 1; border-top-style: dashed; border-top-width: 1; border-bottom-style: dashed; border-bottom-width: 1' width='660' bgcolor='#E2E2E2'>\n";
   echo "<select name='SubmitBy'>\n";
 
     $result = mysql_query("select name from users");
@@ -137,27 +137,11 @@ while ($issues = mysql_fetch_assoc($result)) {
         echo "<option>".$names['name']."</option>\n";
     }
 
-  echo "</select>\n";
-  echo "</td>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='20' bgcolor='$bgcolor'>\n";
-  echo "</td>\n";
-  echo "</tr>\n";
-  echo "<tr>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='40' bgcolor='$bgcolor'>\n";
-  echo "</td>\n";
-  echo "<td valign='top' align='left' style='border-color: #0000FF; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='660' bgcolor='$bgcolor'>\n";
-  echo "<textarea name='Message' rows='5' cols='50' class='field'></textarea>\n";
-  echo "</td>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='20' bgcolor='$bgcolor'>\n";
-  echo "</td>\n";
-  echo "</tr>\n";
-  echo "<tr>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='40' bgcolor='$bgcolor'>\n";
-  echo "</td>\n";
-  echo "<td valign='top' align='left' style='border-color: #0000FF; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='660' bgcolor='$bgcolor'>\n";
+  echo "</select><br>\n";
+  echo "<textarea name='Message' rows='5' cols='50' class='field'></textarea><br><br>\n";
   echo "<br><input type='submit' value='Submit' class='btn'>\n";
   echo "</td>\n";
-  echo "<td valign='middle' align='left' style='border-color: #000000; border-left-style: none; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='20' bgcolor='$bgcolor'>\n";
+  echo "<td valign='top' align='left' style='border-color: #000000; border-left-style: dashed; border-left-width: 0; border-right-style: none; border-right-width: 0; border-top-style: none; border-top-width: 0; border-bottom-style: none; border-bottom-width: 0' width='20' bgcolor='$bgcolor'><img src='../image/delete_comment.png'>\n";
   echo "</td>\n";
   echo "</tr>\n";
   echo "</table>\n";
