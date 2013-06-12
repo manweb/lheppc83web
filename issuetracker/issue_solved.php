cccd09a0eb016d8dec507fc12f4a96ca407c8afd
manweb <manuel.weber@lhep.unibe.ch>
2013-06-12 09:12:09 +0200

    Update.

---
+++ issuetracker/issue_solved.php
@@ -0,0 +1,13 @@
+<?php
+    
+// connect with database
+$db=mysql_connect("lheppc90.unibe.ch","exodaq","EXOsql");
+mysql_select_db("exo");
+
+$id = $_GET['id'];
+
+$delete = mysql_query("update issuetracker set category='2' where ID='$id'");
+
+echo "<meta http-equiv='refresh' content='0; URL=../index.php?page=issuetracker/IssueTracker.php'>";
+
+?>
